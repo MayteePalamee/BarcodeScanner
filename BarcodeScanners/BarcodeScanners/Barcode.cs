c95e6e0ade037c797dcd6bf17e2ca90238020414
MayteePalamee <m.palami@hotmail.com>
2018-07-16 14:06:05 +0700

    Initial commit

---
+++ BarcodeScanners/BarcodeScanners/Barcode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeScanners
+{
+    public class Barcode
+    {
+        /**
+      * event delegate.
+      * Declare the event using EventHandler<T>
+      **/
+
+        public event EventHandler<Events> RaiseEvents;
+
+        /**
+         * delegate method handle raise event
+         **/
+        protected virtual void OnRaiseEvent(Events e)
+        {
+            RaiseEvents?.Invoke(this, e);
+        }
+
+        public Boolean Connect()
+        {
+            try
+            {
+
+            }catch(Exception e)
+            {
+
+            }
+            return true;
+        }
+    }
+}
---
+++ BarcodeScanners/BarcodeScanners/Response.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeScanners
+{
+    public class Response
+    {
+        private String barCodeVID;
+        private String message;
+
+        public String BarCodeID
+        {
+            get { return barCodeVID; }
+            set { barCodeVID = value; }
+        }
+        public String Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+    }
+}
